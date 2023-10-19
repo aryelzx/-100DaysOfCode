1b08b48b5e519a87f31738538b1e5894150cddfc
aryelzx <arielcearamor102@gmail.com>
2023-10-19 19:16:05 -0300

    Day 1 - feat: create a global routes and mock data and controller

---
+++ mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Aryel'
+  },
+  {
+    id: 2,
+    name: 'Ary'
+  },
+  {
+    id: 3,
+    name: 'Maria'
+  },
+  {
+    id: 4,
+    name: 'John Doe'
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const http = require('http');
 
+const routes = require('./routes')
+
 const port = 3000;
 const path = 'http://localhost'
+
+
 const server = http.createServer((req, res) => {
   console.log(`Request method: ${req.method} | endpoint ${req.url}`)
 
-  res.writeHead(200, { "Content-type": "application/json" }),
-    res.end('rodou')
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === req.url && routeObj.method === req.method
+  ));
+
+  if (route) {
+    route.handler(req, res);
+  } else {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end(`Cannot ${req.method} ${req.url}`)
+  }
+
 })
 
 server.listen(port, () => console.log(`server running on ${path}:${port}`))
