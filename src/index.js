f6f4caf5c0917cc78a30ac020749e110f3193ab4
Aryel Ramos <aryel.goncalves@livsaude.com.br>
2023-10-20 09:02:01 -0300

    Day 2 - feat: create a dinamic query params

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const http = require('http');
+const { URL } = require('url');
 
 const routes = require('./routes')
 
 const port = 3000;
-const path = 'http://localhost'
+const local = 'http://localhost'
 
 
 const server = http.createServer((req, res) => {
-  console.log(`Request method: ${req.method} | endpoint ${req.url}`)
+  const parsedUrl = new URL(`http://localhost:3000${req.url}`);
+
+  console.log(`Request method: ${req.method} | endpoint ${parsedUrl.pathname }`)
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === req.url && routeObj.method === req.method
+    routeObj.endpoint === parsedUrl.pathname  && routeObj.method === req.method
   ));
 
   if (route) {
+    req.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(req, res);
   } else {
     res.writeHead(404, { 'Content-type': 'text/html' });
-    res.end(`Cannot ${req.method} ${req.url}`)
+    res.end(`Cannot ${req.method} ${parsedUrl.pathname }`)
   }
 
 })
 
-server.listen(port, () => console.log(`server running on ${path}:${port}`))
+server.listen(port, () => console.log(`server running on ${local}:${port}`))
